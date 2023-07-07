df554f1e88b8f43348c39a79eb6122bd9b648a3d
Vraj1103 <svraj073@gmail.com>
2023-07-07 16:22:03 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());    
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
