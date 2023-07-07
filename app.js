8479d4ab0c0ae440f809eb99d87959bb881bf286
Vraj1103 <svraj073@gmail.com>
2023-07-07 16:58:55 +0530

    Mongo DB connected

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());    
 
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());    
+
+app.get('/', (req, res) => {
+    res.render('home'); 
+});
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping!' });
+    await camp.save();
+    res.send(camp);
+});
 
 
 app.listen(3000, () => {
